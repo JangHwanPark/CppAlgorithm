f9e33a408ab4d504a787366d3f6ef428d0cda379
japark <wkd5878@gmail.com>
2024-02-01 10:50:25 +0900

    [BOJ] 11816(해결)

--- 0001_BOJ/11816.cpp
+++ 0001_BOJ/11816.cpp
@@ -1,7 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(void) {
-    int x;
+    string x;
     cin >> x;
-    cout << x;
+    if (x.length() > 1) {
+        if (x.substr(0, 2) == "0x") {
+            cout << stoi(x, nullptr, 16);
+        } else if (x.substr(0, 1) == "0") {
+            cout << stoi(x, nullptr, 8);
+        } else {
+            cout << x;
+        }
+    }
 }
