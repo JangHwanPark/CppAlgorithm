b542e45bc021ba8edf55cc22d52da1f8756ad628
JangHwanPark <wkd5878@gmail.com>
2024-02-27 21:20:47 +0900

    [BOJ] 1213(frequency)

--- 0000_basic/01_frequency_pattem.cpp
+++ 0000_basic/01_frequency_pattem.cpp
@@ -4,19 +4,56 @@
 // #include <string>
 using namespace std;
 
-int main() {
-    string str = "AABBCCDD";
+map<char, int> calculateFrequency(const string& str) {
     map<char, int> frequency;
 
     // 문자열의 각 문자에 대해 빈도수 계산
-    for (char ch : str) {
-        frequency[ch]++;
+    for (char ch : str) frequency[ch]++;
+
+    return frequency;
+}
+
+bool canFormPalindrome(const map<char, int>& frequency) {
+    int oddCount = 0;
+
+    for (const auto& pair : frequency) {
+        if (pair.second % 2 != 0) {
+            oddCount++;
+
+            if (oddCount > 1) return false;
+        }
     }
+
+    return true;
+}
+
+string fromPalindrome(const map<char, int>& frequency) {
+    string mid = "";
+    string half = "";
+
+    for (const auto& pair : frequency) {
+        half += string(pair.second / 2, pair.first);
+        if (pair.second % 2 != 0) mid = pair.first;
+    }
+
+    string reverseHalf = half;
+    reverse(reverseHalf.begin(), reverseHalf.end());
+    return half + mid + reverseHalf;
+}
+
+int main() {
+    string str = "AABBCCDD";
+    map<char, int> frequency = calculateFrequency(str);
 
     // 빈도수 출력
     for (auto& pair : frequency) {
         cout << pair.first << ": " << pair.second << " " << "\n";
     }
+
+    if (canFormPalindrome(frequency)) {
+        string palindrome = fromPalindrome(frequency);
+        cout << "Palindrome : " << palindrome << "\n";
+    } else cout << "Cannot from a palindrome\n";
 
     return 0;
 }
--- 0001_BOJ/1213_creating_palindrome.cpp
+++ 0001_BOJ/1213_creating_palindrome.cpp
@@ -1,8 +1,39 @@
 // 입력받은 문자열을 팰린드롬으로 만들어야함.
 #include <bits/stdc++.h>
 using namespace std;
-string s;
-char c[50];
-int main() {
 
+pair<bool, string> createPalindrome(const string& input) {
+    map<char, int> frequency;
+    for (char ch : input) frequency[ch]++;
+
+    int oddCount = 0;
+    char oddChar = 0;
+    string halfPalindrome;
+
+    for (const auto& p : frequency) {
+        if (p.second % 2 != 0) {
+            oddCount++;
+            oddChar = p.first;
+            if (oddCount > 1) return { false, "" };
+        }
+
+        halfPalindrome += string(p.second / 2, p.first);
+    }
+
+    string palindrome = halfPalindrome;
+    if (oddCount == 1) palindrome += oddChar;
+    reverse(halfPalindrome.begin(), halfPalindrome.end());
+    palindrome += halfPalindrome;
+
+    return { true, palindrome };
+}
+
+int main() {
+    string input;
+    cin >> input;
+    auto [canCreate, palindrome] = createPalindrome(input);
+
+    if (canCreate) cout << palindrome;
+    else cout << "I'm Sorry Hansoo";
+    return 0;
 }
