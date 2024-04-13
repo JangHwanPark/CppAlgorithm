d8e54867c13fdeb194554df4177604de0ebb1280
JangHwanPark <wkd5878@gmail.com>
2024-04-13 21:04:20 +0900

    Study : String

--- 00_basic/exam01.cpp
+++ 00_basic/exam01.cpp
@@ -1,15 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node {
+struct StructNode {
     string data;
-    Node *next;
+    StructNode *next{};
+};
+
+class ClassNode {
+public:
+    int data;
+    ClassNode* next;
+
+    // 생성자
+    ClassNode(int data) {
+        this->data = data;      // data 변수에 매개변수 data 할당
+        this->next = nullptr;   // next 변수에 null 포인터 할당(null 값)
+    }
 };
 
 
 int main() {
-    Node node;
+    StructNode node;
     node.data = "data1";
-    cout << &node.data << '\n';
+    cout << &node.data << " " << node.data << " Type: " << typeid(node.data).name() << '\n';
+
+    StructNode node2;
+    node2.data = "data2";
+    cout << &node2.data << " " << node2.data << " Type: " << typeid(node2.data).name() << '\n';
+
+    // ClassNode의 타입 정보를 출력
+    ClassNode cnode(100);
+    cout << "ClassNode data type: " << typeid(cnode.data).name() << '\n';
+
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
-int n, m, res, temp;
+
 int main() {
-    cin >> n;
-    int t[n];
-    for (int i = 0; i < n; i++) {
-        cin >> m;t[i] = m;
+    int tc; // 테스트 케이스의 수
+    cin >> tc; // 테스트 케이스 수 입력 받기
+    for (int i = 1; i <= tc; ++i) {
+        int b; // 변환할 바이트 수
+        cin >> b; // 바이트 수 입력 받기
+        string s; // 입력 문자열
+        cin >> s; // 문자열 S 입력 받기
+
+        string res; // 최종 문자열 결과
+        for (int i = 0; i < 8 * b; i += 8) {
+            string binaryStr; // 이진수 문자열
+            for (int j = i; j < i + 8; ++j) {
+                binaryStr += (s[j] == 'I' ? '1' : '0');
+            }
+            bitset<8> bits(binaryStr); // 문자열을 bitset으로 변환
+            char convertedChar = static_cast<char>(bits.to_ulong()); // bitset을 unsigned long으로 변환 후 char로 캐스팅
+            res += convertedChar; // 결과 문자열에 추가
+        }
+
+        cout << "Case #" << i << ": " << res << "\n"; // 결과 출력
     }
-    sort(t, t + n);
-    for (int i : t) {
-        temp += i;res += temp;
-    }
-    cout << res;
     return 0;
 }
